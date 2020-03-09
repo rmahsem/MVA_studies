446d90ba5df5676446338e440d4270b2a0a0b24a
Dylan Teague <dylan.o.teague@vanderbilt.edu>
2020-03-09 15:12:03 +0100

    fixing xgboost code to work under tmva workflow. Still needs opt

--- Utilities/treeGroup.py
+++ Utilities/treeGroup.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 import xgboost as xgb
 import numpy as np
 
-class treeGroup:
+
+class TreeGroup:
     bins=np.linspace(0,1,11)
+    bNames = []
+    exclude = ["isSignal", "weight", "newWeight", "genWeight"]
     def __init__(self, infile, label, xsec, isSig=False):
+        
         self.f = list()
         removeList = list()
         for dirN in label:
-            dataF = self.getFrameYear(infile, dirN, isSig)
+            dataF = self.getFrame(infile, dirN, isSig)
             if dataF.empty:
                 removeList.append(dirN)
             else:
@@ -18,11 +22,8 @@ class treeGroup:
             label.remove(dirN)
         self.xsec = xsec
         self.label = label
-        self.sumweight = [self.getSumWeight(infile, dirN) for dirN in label]
-        self.extraWeight = np.ones(len(self.f))
-        for frame,xs,sw in zip(self.f, self.xsec, self.sumweight):
-            frame["weight"] *= xs/sw
-            
+        self.testFact = np.ones(len(self.f))
+        
         self.histOpt = {"stacked":True, "alpha":1.0, "edgecolor":'black', "histtype":'stepfilled'}
         self.madeWeight = False
         
@@ -30,8 +31,7 @@ class treeGroup:
         self.f.extend(other.f)
         self.xsec.extend(other.xsec)
         self.label.extend(other.label)
-        self.sumweight.extend(other.sumweight)
-        self.extraWeight = np.concatenate([self.extraWeight, other.extraWeight])
+        self.testFact = np.concatenate([self.testFact, other.testFact])
         self.pred.extend(other.pred)
         self.madeWeight = False
         return self
@@ -43,62 +43,24 @@ class treeGroup:
         return self.f[index]
 
     def makePred(self, func):
-        self.pred = [func.predict(xgb.DMatrix(frame.drop(["isSignal","weight"], axis=1))) for frame in self.f]
+        self.pred = [func.predict(xgb.DMatrix(frame.drop(TreeGroup.exclude, axis=1))) for frame in self.f]
 
     def makeWgt(self, lumi=140000):
-        self.wgt = [mframe["weight"].values*mExtraWgt*lumi for mframe, mExtraWgt in zip(self.f, self.extraWeight)]
+        self.wgt = [mframe["newWeight"].values*mExtraWgt*lumi for mframe, mExtraWgt in zip(self.f, self.testFact)]
         self.madeWeight = True
         # for name, wgt in zip(self.label, self.wgt):
         #     print(name, sum(wgt))
 
-    def getSumWeight(self, infile, dirName):
-        if dirName in infile:
-            return infile[dirName]["sumweights"].values.sum()
-        sumTot = 0
-        for year in ["2016", "2017", "2018"]:
-            if "%s%s" % (dirName, year) in infile:
-                sumTot += infile["%s%s" % (dirName, year)]["sumweights"].values.sum()
-        return sumTot
-
-            
-    def getFrameYear(self, infile, dirName, signal):
-        frame = self.getFrame(infile, dirName, signal)
-        if not frame.empty:
-            return frame
-        frameList = list()
-        for year in ["2016", "2017", "2018"]:
-            frameTmp = self.getFrame(infile, "%s%s" % (dirName, year), signal)
-            if not frameTmp.empty: frameList.append(frameTmp)
-        if not frameList:
-            return pandas.DataFrame()
-
-        frame = pandas.concat(frameList, ignore_index=True)
-        if not frame.empty:
-            return frame
-        else:
-            print( "PROBLEM: %s" % dirName)
-            exit()
-    
     def getFrame(self, infile, dirName, signal):
-        global bNames
-        if dirName not in infile:
-            return pandas.DataFrame()
-        dir = infile[dirName]
-        i=1
-        valid = list()
-        while("testTree;%i"%i in dir):
-            valid.append(dir["testTree;%i"%i])
-            i += 1
-        frames = [tree.pandas.df(bNames) for tree in valid]
-        frame = pandas.concat(frames, ignore_index=True)
+        frame = infile[dirName].pandas.df(TreeGroup.bNames+TreeGroup.exclude[1:])
         if signal:
             frame["isSignal"] = 1
         else:
             frame["isSignal"] = 0
-            #Cuts
+        #Cuts
         frame = frame[frame["HT"] > 150]
         # frame = frame[frame["NJets"] >= 2]
-        frxame = frame[frame["MET"] > 25]
+        frame = frame[frame["MET"] > 25]
         frame = frame[frame["DilepCharge"] > 0]
         # frame = frame[frame["DilepCharge"] < 0]
         frame = frame.reset_index(drop=True)
@@ -106,11 +68,11 @@ class treeGroup:
 
     def makeTest(self, num, idx=0):
         tot = 1.0*len(self.f[idx])
-        self.extraWeight[idx] *= tot
+        self.testFact[idx] *= tot
         train_return = self.f[idx].truncate(after=num-1)
         train_return["weight"] *= tot/len(train_return)
         self.f[idx] = self.f[idx].truncate(before=num)
-        self.extraWeight[idx] *= 1./len(self.f[idx])
+        self.testFact[idx] *= 1./len(self.f[idx])
         print( "Train %s with %d" % (self.label[idx], num))
         return train_return
 
@@ -120,7 +82,7 @@ class treeGroup:
         if sc >0:
             wgt = [inWgt*sc for inWgt in wgt]
         lab = [name+exLabel for name in self.label]
-        return  plt.hist(self.pred, treeGroup.bins, weights=wgt, label=lab, density=isDen, **self.histOpt)
+        return  plt.hist(self.pred, TreeGroup.bins, weights=wgt, label=lab, density=isDen, **self.histOpt)
         
     def getHist(self, bins):
         if not self.madeWeight: self.makeWgt(lumi=140000)
@@ -135,6 +97,4 @@ class treeGroup:
         total_arr = [np.sum(wgt) for wgt in self.wgt]
         return np.sum(total_arr)
 
-bNames = ["NJets", "NBJets", "HT", "MET", "l1Pt", "l2Pt", "lepMass", "centrality", \
-          "j1Pt", "j2Pt", "j3Pt", "j4Pt", "j5Pt", "j6Pt", "j7Pt", "j8Pt", "jetMass", "jetDR", \
-          "b1Pt", "b2Pt", "b3Pt", "b4Pt", "Shape1", "Shape2", "DilepCharge", "NlooseBJets", "NtightBJets", "NlooseLeps", "weight",]
+
--- mva_test.py
+++ mva_test.py
@@ -4,20 +4,65 @@ import xgboost as xgb
 import warnings
 warnings.filterwarnings("ignore")
 import Utilities.helper as helper
+from Utilities.treeGroup import TreeGroup
+import uproot, os, argparse, logging
 
+parser = argparse.ArgumentParser(description='Run TMVA over 4top')
+parser.add_argument('-f', '--infile', required=True, type=str, help='Infile/outfile name')
+parser.add_argument('--mva', action="store_true", help='Run TMVA along with make plots')
+parser.add_argument('--useNeg', action="store_true", help='Use Negative event weights in training')
+parser.add_argument('--show', action="store_true", help='Set if one wants to see plots when they are made (default is to run in batch mode)')
+parser.add_argument('-l', '--lumi', type=float, default=140., help='Luminosity to use for graphs given in ifb: default 140')
+parser.add_argument('--debug', action="store_true", help="Add 'debug' flag to get more information")
 
+args = parser.parse_args()
+
+bNames = ["NJets", "NBJets", "HT", "MET", "l1Pt", "l2Pt", "lepMass", "centrality", "j1Pt", "j2Pt", "j3Pt", "j4Pt", "j5Pt", "j6Pt", "j7Pt", "j8Pt", "jetMass", "jetDR", "b1Pt", "b2Pt", "b3Pt", "b4Pt", "Shape1", "Shape2", "DilepCharge", "NlooseBJets", "NtightBJets", "NlooseLeps"]
 
 #inputs to change
 bins = np.linspace(0,1,41)
-doShow = False
-saveDir = "3top_blah"
 saveModelName = None
+helper.saveDir = args.infile
+helper.doShow = args.show
+TreeGroup.bNames = bNames
+TreeGroup.bins = bins
+logging.basicConfig(level=(logging.INFO if args.debug else logging.WARNING))
 
+if not os.path.isdir(args.infile):
+    os.mkdir(args.infile)
 
-helper.saveDir = saveDir
-helper.doShow = doShow
-from Utilities.samples import *
+infile = uproot.open("inputTrees.root")
 
+top3SF = 1.0
+top4_g = TreeGroup(infile, ["4top2016"], [0.0092], False)
+top3_g = TreeGroup(infile, ["tttj", "tttw"], [top3SF*0.000474, top3SF*0.000788], True)
+
+signal_g = top3_g
+signal_g.histOpt["alpha"] = 0.5
+other_g = top4_g
+
+ttv_g = TreeGroup(infile,
+                  ["ttw", "ttz", "tth2nonbb"],
+                  [0.2043, 0.2529, 0.2151])
+ttvv_g = TreeGroup(infile,
+                  ["ttwh", "ttwz", "ttww", "tthh", "ttzh", "ttzz"],
+                  [0.001582, 0.003884, 0.01150, 0.000757, 0.001535, 0.001982])
+vvv_g = TreeGroup(infile,
+                   ["www", "wwz", "wzz", "zzz"],
+                   [0.2086, 0.1651, 0.5565, 0.01398])
+vv_g = TreeGroup(infile,
+                 ["zz4l_powheg", "wz3lnu_mg5amcnlo", "ww_doubleScatter", "wpwpjj_ewk", "vh2nonbb"],
+                 [1.256, 4.4297, 0.16975, 0.03711, 0.9561])
+xg_g = TreeGroup(infile,
+                 ["ttg_dilep", "wwg", "wzg", "zg", "ttg_lepfromTbar", "ttg_lepfromT", "ggh2zz", "wg"],
+                 [0.632, 0.2147, 0.04123, 123.9, 0.769, 0.77, 0.01181, 405.271])
+extra_g = TreeGroup(infile,
+                    ["tzq", "st_twll", "DYm50", "DYm10-50", "wjets"],#"ttbar", 831.762,
+                    [0.0758, 0.01123, 6020.85,  18610, 61334.9])
+
+all_samples = [signal_g, other_g, ttv_g, ttvv_g, vvv_g, vv_g, xg_g, extra_g]
+all_bkg = [other_g, ttv_g, ttvv_g, vvv_g, vv_g, xg_g, extra_g]
+special_samples = [signal_g, other_g]
 
 # Create Train and Test groups
 split_train = list()
@@ -31,69 +76,69 @@ for sample in special_samples:
             split_train.append(sample.makeTest(nEvt/6,i))
 
 for frame in all_samples:
-    print([(name, len(i)) for i, name in zip(frame.f, frame.label)])
-print()
+    logging.info([(name, len(i)) for i, name in zip(frame.f, frame.label)])
+print 
+
+
+if args.mva:
+    # Setup XGBoost stuff
+    X_train = pandas.concat(split_train,ignore_index=True).drop(TreeGroup.exclude, axis=1)
+    y_train = pandas.concat(split_train, ignore_index=True)["isSignal"]
+
+    scalefact = 1.0*np.sum(y_train)/len(y_train)
+    scale_train = np.where(y_train, 1, scalefact)
+
+    sig_wgt, bkg_wgt = list(), list()
+    for fr in split_train:
+        if np.unique(fr["isSignal"]) == 1:
+            sig_wgt.append(np.sum(fr["weight"]))
+        else: 
+            bkg_wgt.append(np.sum(fr["weight"]))
+
+
+    #weight nonsense
+    # use a "mean xsec" method to keep weights O(1) while including relative weights between xsec
+    sig_wgt = np.array(sig_wgt)
+    bkg_wgt = np.array(bkg_wgt)
+    tot_mean_wgt = np.concatenate([sig_wgt/np.mean(sig_wgt), bkg_wgt/np.mean(bkg_wgt)])
+    w_train = np.concatenate([wgt*np.ones(len(frame)) for frame, wgt in zip(split_train, tot_mean_wgt)])
+
+    print( "In weights:", np.unique(w_train))
 
 
 
-# Setup XGBoost stuff
-X_train = pandas.concat(split_train,ignore_index=True).drop(["isSignal", "weight"], axis=1)
-y_train = pandas.concat(split_train, ignore_index=True)["isSignal"]
+    # XGBoost training
+    dtrain = xgb.DMatrix(X_train, label=y_train, weight=w_train)
 
-scalefact = 1.0*np.sum(y_train)/len(y_train)
-scale_train = np.where(y_train, 1, scalefact)
-
-sig_wgt, bkg_wgt = list(), list()
-for fr in split_train:
-    if np.unique(fr["isSignal"]) == 1:
-        sig_wgt.append(np.sum(fr["weight"]))
-    else: 
-        bkg_wgt.append(np.sum(fr["weight"]))
-
-
-#weight nonsense
-# use a "mean xsec" method to keep weights O(1) while including relative weights between xsec
-sig_wgt = np.array(sig_wgt)
-bkg_wgt = np.array(bkg_wgt)
-tot_mean_wgt = np.concatenate([sig_wgt/np.mean(sig_wgt), bkg_wgt/np.mean(bkg_wgt)])
-w_train = np.concatenate([wgt*np.ones(len(frame)) for frame, wgt in zip(split_train, tot_mean_wgt)])
-
-print( "In weights:", np.unique(w_train))
-
-
-
-# XGBoost training
-dtrain = xgb.DMatrix(X_train, label=y_train, weight=w_train)
-
-evallist  = [(dtrain,'train')]
-num_round=200
-param = {}
-param['objective'] = 'binary:logistic'
-param['eta'] = 0.09
-param['max_depth'] = 5
-param['silent'] = 1
-param['nthread'] = 2
-param['eval_metric'] = "auc"
-param['subsample'] = 0.9
-param['colsample_bytree'] = 0.5
-fitModel = xgb.train(param.items(), dtrain, num_round, evallist,early_stopping_rounds=100, verbose_eval=100 )
-
-if saveModelName:
-    fitModel.save_model(saveModelName)
+    evallist  = [(dtrain,'train')]
+    num_round=200
+    param = {}
+    param['objective'] = 'binary:logistic'
+    param['eta'] = 0.09
+    param['max_depth'] = 5
+    param['silent'] = 1
+    param['nthread'] = 2
+    param['eval_metric'] = "auc"
+    param['subsample'] = 0.9
+    param['colsample_bytree'] = 0.5
+    fitModel = xgb.train(param.items(), dtrain, num_round, evallist,early_stopping_rounds=100, verbose_eval=100 )
     
+    fitModel.save_model("{}/model.bin".format(args.infile))
+
+
+
+loadModel = xgb.Booster({'nthread': 4})
+loadModel.load_model("{}/model.bin".format(args.infile))
+
 for frame in all_samples:
-    frame.makePred(fitModel)
+    frame.makePred(loadModel)
 
-    
 # Setting up background
 bkg_g = None
 from copy import deepcopy
-for frame in [other_g, ttv_g, ttvv_g, vvv_g, vv_g, xg_g, extra_g]:
-    if bkg_g:
-        bkg_g += frame
-    else:
-        
-        bkg_g = deepcopy(frame)
+for frame in all_bkg:
+    if bkg_g:  bkg_g += frame
+    else:      bkg_g = deepcopy(frame)
 
 # Make Plots
 
@@ -113,20 +158,18 @@ helper.StoB(signal_g.getHist(fineBin), other_g.getHist(fineBin), fineBin, "4top"
 helper.StoB(signal_g.getHist(fineBin), bkg_g.getHist(fineBin), fineBin, "All", noSB=True)
 
 # Get approximated Likelihood values
-
-print()
-print( "Signal to similar: ", helper.approxLikelihood(signal_g.getHist(bins), other_g.getHist(bins)))
-print( "Signal to all Bkg: ", helper.approxLikelihood(signal_g.getHist(bins), bkg_g.getHist(bins)))
-
+print( "Signal to similar: {}".format(helper.approxLikelihood(signal_g.getHist(bins), other_g.getHist(bins))))
+print( "Signal to all Bkg: {}".format(helper.approxLikelihood(signal_g.getHist(bins), bkg_g.getHist(bins))))
+print 
 helper.makeROC(special_samples, "3vs4")
 helper.makeROC(all_samples, "all")
 
 # sorted usefulness of variables
 
-fscoreFunc = fitModel.get_fscore().get
-for rank,name in sorted([(fscoreFunc(name) if fscoreFunc(name) else 0, name) for name in fitModel.feature_names], reverse=True):
-    print( "{:4s} {:15s}".format(str(rank),str(name)))
-print()
+fscoreFunc = loadModel.get_fscore().get
+for rank,name in sorted([(fscoreFunc(name) if fscoreFunc(name) else 0, name) for name in loadModel.feature_names], reverse=True):
+    print("{:4s} {:15s}".format(str(rank),str(name)))
+
 
  
 
